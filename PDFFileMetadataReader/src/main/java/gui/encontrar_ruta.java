46cf183196aac617a57991e9ac632b5fc5f9b0d4
Estuardo Gutiérrez <EAGUTIERREZTZ@correo.url.edu.gt>
2023-10-10 20:08:33 -0600

    Guardar la ruta

--- PDFFileMetadataReader/src/main/java/gui/encontrar_ruta.java
+++ PDFFileMetadataReader/src/main/java/gui/encontrar_ruta.java
@@ -1,7 +1,11 @@
 
 package gui;
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class encontrar_ruta {
     public void find(){
@@ -12,9 +16,22 @@ public class encontrar_ruta {
         
         if (result == JFileChooser.APPROVE_OPTION) {
                     String Path = fileChooser.getSelectedFile().getAbsolutePath();
+                    escribir(Path);
                     System.out.println("Archivo de ruta: " + Path);
+                    
                 } else {
                     System.out.println("No se ha seleccionado.");
                 }  
+    }
+    public void escribir(String ruta) {
+        try{
+            FileOutputStream archivo = new FileOutputStream("Prueba.txt", true);
+            try (DataOutputStream writer = new DataOutputStream(archivo)) {
+                writer.writeUTF(ruta);
+                System.out.println("Se ha creado");
+            }
+        }catch (IOException e){
+            JOptionPane.showMessageDialog(null, "Ha ocurrido un error", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
